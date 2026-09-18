25d4b552db18669b858294382483b5033eec174f
faijisani <isanifaij@gmail.com>
2026-09-18 18:20:57 +0530

    Create src/pages/Apply.jsx

---
+++ src/src/pages/Apply.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ApplicationForm from "../components/ApplicationForm";
+
+function Apply() {
+  return (
+    <section>
+      <ApplicationForm />
+    </section>
+  );
+}
+
+export default Apply;
