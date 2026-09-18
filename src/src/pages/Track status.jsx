d2132e5dc9aef2c74503808ad6d0caeaa958f245
faijisani <isanifaij@gmail.com>
2026-09-18 18:21:51 +0530

    Create src/pages/Track status.jsx

---
+++ src/src/pages/Track status.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StatusTracker from "../components/StatusTracker";
+
+function TrackStatus() {
+  return (
+    <section>
+      <StatusTracker />
+    </section>
+  );
+}
+
+export default TrackStatus;
