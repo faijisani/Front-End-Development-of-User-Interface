a71f3d054aa0c993f9ef19a60f51365b861f50d8
faijisani <isanifaij@gmail.com>
2026-09-18 18:09:25 +0530

    Create src/components/Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>🏛️ E-Governance Portal</h1>
+      <p>Digital Government Services at Your Fingertips</p>
+    </header>
+  );
+}
+
+export default Header;
